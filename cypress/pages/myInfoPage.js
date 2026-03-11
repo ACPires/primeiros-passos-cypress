266e29bd2c10563936843550afa1408ceaa411ec
ACPires <arthur98_4@hotmail.com>
2026-03-11 00:38:44 -0300

    Updated my info variables

--- cypress/pages/myInfoPage.js
+++ cypress/pages/myInfoPage.js
@@ -1,6 +1,6 @@
 class MyInfoPage {
     selectorsList() {
-        selectors ={
+        const selectors = {
             myInfoMenu: '[href="/web/index.php/pim/viewMyDetails"]',
             firstNameField: '[name="firstName"]',
             lastNameField: '[name="lastName"]',
@@ -18,16 +18,16 @@ class MyInfoPage {
     }
 
     updateFields(firstName,lastName,nickname,eId,oId,driversLicense,licenseExpireDate,sSN,sIN){
-        cy.get(this.selectorsList().firstNameField).clear().type('Joaquim')
-        cy.get(this.selectorsList().lastNameField).clear().type('Albuquerque')
-        cy.get(this.selectorsList().activeInputField).eq(3).clear().type('Joque')   //Nickname
-        cy.get(this.selectorsList().activeInputField).eq(4).clear().type('2256')    //Employee ID (max 10)
-        cy.get(this.selectorsList().activeInputField).eq(5).clear().type('1234')    //Other ID
-        cy.get(this.selectorsList().activeInputField).eq(6).clear().type('945682N') //Driver's License Number (max 30)
-        cy.get(this.selectorsList().dateInputField).eq(0).clear().type('2029-02-24')//License Expire Date
+        cy.get(this.selectorsList().firstNameField).clear().type(firstName)
+        cy.get(this.selectorsList().lastNameField).clear().type(lastName)
+        cy.get(this.selectorsList().activeInputField).eq(3).clear().type(nickname)   //Nickname
+        cy.get(this.selectorsList().activeInputField).eq(4).clear().type(eId)    //Employee ID (max 10)
+        cy.get(this.selectorsList().activeInputField).eq(5).clear().type(oId)    //Other ID
+        cy.get(this.selectorsList().activeInputField).eq(6).clear().type(driversLicense) //Driver's License Number (max 30)
+        cy.get(this.selectorsList().dateInputField).eq(0).clear().type(licenseExpireDate)//License Expire Date
         cy.get(this.selectorsList().dateCloseButton).click()    
-        cy.get(this.selectorsList().activeInputField).eq(8).clear().type('5725')    //SSN
-        cy.get(this.selectorsList().activeInputField).eq(9).clear().type('3216')    //SIN
+        cy.get(this.selectorsList().activeInputField).eq(8).clear().type(sSN)    //SSN
+        cy.get(this.selectorsList().activeInputField).eq(9).clear().type(sIN)    //SIN
         cy.get(this.selectorsList().submitButton).eq(0).click()
     }
 
